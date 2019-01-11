895453cb4e767821871b39a01f7208104d9e609d
Dominic <dvphan@ucdavis.edu>
2019-01-11 11:24:25 -0800

    added index to component directory

--- homies-blog/src/index.js
+++ homies-blog/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import { HomePage } from './components';
 ReactDOM.render(
   <BrowserRouter>
     <div>
