1efc487e584ad66e2ae5654f0d55466bbf263930
Dominic <dvphan@ucdavis.edu>
2019-01-17 09:54:04 -0800

    added backend

--- client/homies-blog/src/components/BlogFeed.js
+++ client/homies-blog/src/components/BlogFeed.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BlogPost } from './';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import styled from 'styled-components';
 
